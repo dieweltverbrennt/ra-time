0c196333a3627d0cb2b12fbd4e80da538d3c2cae
Maria <nikmaria15@gmail.com>
2023-07-18 10:34:54 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
+// import DateTimePretty from './Components/DateTimePretty';
+import moment from "moment";
+
+const dateTimePretty = (Component) => {
+    return function (props) {
+        return <Component {...props} date={moment(props.date).fromNow()} />
+    };
+};
+
+const DateTimePretty = dateTimePretty(DateTime);
+
+function DateTime(props) {
+  console.log(props.date)
+    return (
+        <p className="date">{props.date}</p>
+    )
+}
+
+function Video(props) {
+    return (
+        <div className="video">
+            <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+            <DateTimePretty date={props.date} />
+        </div>
+    )
+}
+
+function VideoList(props) {
+    return props.list.map(item => <Video key={uuidv4()} url={item.url} date={item.date} />);
+}
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-07-31 13:24:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-03-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-02-03 23:16:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/RK1K2bCg4J8?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-03 12:10:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2018-01-01 16:17:00'
+        },
+        {
+            url: 'https://www.youtube.com/embed/TxbE79-1OSI?rel=0&amp;controls=0&amp;showinfo=0',
+            date: '2017-12-02 05:24:00'
+        },
+    ]);
+
+    return (
+        <VideoList list={list} />
+    );
+}
---
+++ src/Components/DateTimePretty.js
@@ -0,0 +1,10 @@
+import moment from "moment";
+
+const DateTimePretty = (Component) => {
+    return function (props) {
+        return <Component {...props} date={moment(props.date).fromNow()} />
+    };
+};
+
+export default DateTimePretty;
+
